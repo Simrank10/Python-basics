4e86f8d7b195bf1e07332d8dc90650f694c2433e
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-14 20:48:56 +0530

    Add files via upload

---
+++ variables_and_len.py
@@ -0,0 +1,21 @@
+Name = input("What's your name? ")
+print(Name)
+
+
+print()
+Name = "Siya"
+print(Name)
+
+print()
+Name = input("What's your name? ")
+length = len(Name)
+print(length)
+
+print("concatenating strings")
+surname = "Pandey"
+print(Name + " " +surname)
+
+
+print("concatenating strings")
+surname = 123
+print(Name + " " + surname)
