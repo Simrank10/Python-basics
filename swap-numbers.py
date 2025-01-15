4139f392763188f903f6d95132782b1d3565b15b
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-15 23:14:13 +0530

    Add files via upload

---
+++ swap-numbers.py
@@ -0,0 +1,17 @@
+S1_name = "Swapnil"
+print(S1_name)
+
+#"a" = 1
+#print("a")
+
+no1 = input("Enter value for 1st number:")
+no2 = input("Enter value for 2nd number: ")
+print("no1 initially: ", no1)
+print("no2 initially: ", no2)
+
+temp = no1
+no1 = no2
+no2 = temp
+print()
+print("no1 after swap: ", no1)
+print("no2 after swap: ", no2)
