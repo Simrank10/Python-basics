b9324a38146a24c6ef2c68bb07ccf18e09bc70c8
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-16 22:23:13 +0530

    Add files via upload

---
+++ arithmetic-operations.py
@@ -0,0 +1,28 @@
+print()
+print("Arithmetic sum: ", 8 + 2)
+print()
+print("Arithmetic subtraction: ", 8 - 2)
+print()
+print("Arithmetic multiplication: ", 8*2)
+print()
+print("Arithmetic exponential: ", 8**2)
+print()
+print("Arithmetic division: ", 8/2)
+print()
+print("Arithmetic modulus: ", 8%2)
+print()
+print("Arithmetic floor division: ", 8//2)
+print()
+print("using PEMDAS (PARENTHESIS OF EXPONENTIAL, MULTIPLICATION, DIVISION, ADDITION AND SUBTRACTION)")
+print("Solution of 5 + 2 * 3 - 1 + 10/5 : ", 5 + 2 * 3 - 1 + 10/5)
+print()
+print("Solution of 5 + 2 * (3 - 1) + 10/5 : ", 5 + 2 * (3 - 1) + 10/5)
+print()
+
+
+print("BMI calculator")
+height_mtrs = float(input("Enter your height in cms: "))
+height = height_mtrs/100
+weight = int(input("Enter your weight in kgs: "))
+BMI = weight/(height)**2
+print("your BMI is: ", BMI)
