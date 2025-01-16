4e2b50900942700555b78c7f1057001ccec5aab4
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-16 22:24:02 +0530

    Add files via upload

---
+++ logical-operators.py
@@ -0,0 +1,22 @@
+m, n = 12, 21
+print("m = ", m, ", n = ", n)
+print()
+
+print("AND operators in action - ")
+print("m> 12 and n >2 : ", m> 12 and n >2)
+print("m != 10 and n == 10 : ", m != 10 and n == 10)
+print("m <= 100 and n >= 45 : ", m <= 100 and n >= 15)
+print()
+
+print("OR operators in action - ")
+print("m <= 12 or n > 100 : ", m <= 12 or n > 100)
+print("m == 10 or n != 30 : ", m == 10 or n != 30)
+print("m < 5 or n >= 98 : ", m < 5 or n >= 98)
+print()
+
+print("NOT operator in action - ")
+a = True
+print("a = ", a)
+print("not(a) : ", not(a))
+print("m,n and a : ", m< n and a)
+print("m,n and not(a) : ", m< n and not(a))
