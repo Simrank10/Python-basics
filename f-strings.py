44a3b17238dc16cd383aad42f742df830899656a
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-17 23:19:02 +0530

    Add files via upload

---
+++ f-strings.py
@@ -0,0 +1,9 @@
+name = "Amaira Trivedi"
+age = 6
+height = 130
+weight = 35
+
+BMI = weight/((height/100)**2)
+
+print(f"Her name is {name} and she is {age} years old. She is {height} cms tall and weigh {weight} kgs, and her BMI is {int(BMI)}.")
+print(f"{name}'s father is {age*5} years old.")
