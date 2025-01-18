054ed74898394c5b7a01e404444b41e270422b2f
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-18 21:53:41 +0530

    Add files via upload

---
+++ exercise-3.py
@@ -0,0 +1,10 @@
+"""
+Check whether a given number is even or odd.
+"""
+
+num = int(input("Enter a number : "))
+
+if num%2 == 0:
+    print("Entered number is even!")
+else:
+    print("Entered number is odd!")
