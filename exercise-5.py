0df9dcc451f4603bc6b81ba4ae4cb714073da284
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-18 21:54:49 +0530

    Add files via upload

---
+++ exercise-5.py
@@ -0,0 +1,21 @@
+"""
+Check whether a given year is leap year or not?
+"""
+
+year = int(input("Enter a year you want to check for : "))
+print(f"Okay, Let's check for {year} year...")
+
+if year % 4 == 0:
+    if year % 100 ==0:
+        if year % 400==0:
+            print(f"The year {year} is a leap year!")
+        else:
+            print(f"The year {year} is not a leap year.")
+    else:
+        print(f"The year {year} is a leap year.")
+else:
+    print(f"The year {year} is not a leap year!")
+
+print("Leap year is year which is completely divisible by 4. Also, if year is not divisible "
+      " by 100, its a leap year and if divisible by 100 we check whether it is divisible by 400" 
+      " too conclude its a leap year")
