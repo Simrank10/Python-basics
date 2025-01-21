cc28f3de0d672ed855c7ec2bd3084c4b74b12334
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-21 20:23:40 +0530

    Add files via upload

---
+++ split-function.py
@@ -0,0 +1,7 @@
+text = "Welcome to Joey's pizza"
+splitted_text = text.split(" ")
+print(splitted_text)
+
+names = str(input("PLease enter your team members names seperated by comma : "))
+splitted_names = names.split(", ")
+print(splitted_names)
