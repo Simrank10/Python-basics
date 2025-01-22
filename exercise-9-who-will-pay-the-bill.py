41817a6970707dd8cb5e6cfed0828ff83d1035b9
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-22 23:24:49 +0530

    Add files via upload

---
+++ exercise-9-who-will-pay-the-bill.py
@@ -0,0 +1,24 @@
+"""
+WAP which will select a random name name from the list of names and the 
+person selected will have to pay for everybody's food.
+"""
+
+
+import random
+
+names = str(input("Enter the names seperated with commas : "))
+members = names.split(", ")
+
+print(members)
+#print(len(members))
+
+choice = random.randint(0, len(members)-1)
+#print(choice)
+print(f"{members[choice]} will pay the bill!")
+
+"""
+ALTERNATIVELY, use choice!
+"""
+
+person_selected = random.choice(members)
+print(person_selected)
