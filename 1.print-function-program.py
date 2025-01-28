4e3931ce65f8aad9ea28c10a87e78e5295452725
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-28 22:46:17 +0530

    Update 1.print-function-program.py

--- 1.print-function-program.py
+++ 1.print-function-program.py
@@ -1,3 +1,8 @@
 print("First Program - Python print function")
+# OUTPUT - First Program - Python print function                                                                                                                        
+
 print("It is declared like this:")
+# OUTPUT - It is declared like this:                   
+
 print('print("What to print?")')
+# OUTPUT - print("What to print?")
