c31e66942029cffec64ca1f163afef7a11909ea0
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-29 23:21:08 +0530

    Update 3.input_function.py

--- 3.input_function.py
+++ 3.input_function.py
@@ -1,9 +1,11 @@
 input("What is your name?")
 print()
+
+
 # Input function will be executed first and later the Print function will be executed
 print("Hello" + " " + input("What's your name?")) 
 
 
-#Assignment
 
+#Assignment
 print("Hey" + " " + input("What's your name?") + ", " "How are you?")
