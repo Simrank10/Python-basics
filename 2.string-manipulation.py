877eb60cd6ab5e3e947f6b8e6f30d0f0de3ed030
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-29 23:41:29 +0530

    Update 2.string-manipulation.py

--- 2.string-manipulation.py
+++ 2.string-manipulation.py
@@ -1,45 +1,51 @@
+# 1.
 print()
 print("Hello world\nHello world")
-```
+"""
 OUTPUT -
 Hello world
 Hello world
-```
+"""
 
+# 2.
 print()
 print("Hello" + "Amaira")
-```
+"""
 OUTPUT -
 HelloAmaira
-```
+"""
 
+# 3.
 print()
 print("Hello " + "Amaira")
-```
+"""
 OUTPUT -
 Hello Amaira
-```
+"""
 
+# 4.
 print()
 print('Hello' + " Amaira")
-```
+"""
 OUTPUT -
 Hello Amaira
-```
+"""
 
+# 5.
 print()
 print("Hello" + ' ' + "Amaira")
-```
+"""
 OUTPUT -
 Hello Amaira
-```
+"""
 
+# 6.
 print()
 print("Hello, good morning\nHave an amazing day!")
-```
+"""
 OUTPUT -
 Hello, good morning
 Have an amazing day!
-```
+"""
 
 # print("Hello, good morning/nHave an amazing day!")
