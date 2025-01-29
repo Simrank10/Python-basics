bc6c0d02acca6c494f9d71a067560b402a56b82f
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-29 23:38:12 +0530

    Update 5.data-types.py

--- 5.data-types.py
+++ 5.data-types.py
@@ -3,11 +3,12 @@ print("That's how hex and oct num are printed...")
 print(0x123)
 print(0o123)
 
-```python
+"""
+OUTPUT -
 That's how hex and oct num are printed...
 291
 83
-```
+"""
 
 # 2.
 print()
@@ -18,23 +19,25 @@ print(var_1)
 print(var_1 + 193933)
 print(var_1 + 9980.8999)
 
-```python
+"""
+OUTPUT -
 INTEGER NUMBERS
 127482942830429734829891801
 127482942830429734830085734
 1.2748294283042974e+26
-```
+"""
 
 # 3. 
 print(var_2 + 10.99)
 print(var_2 + 10.90)
 print(var_1 + var_2)
 
-```python
+"""
+OUTPUT -
 110.99
 110.9
 127482942830429734829891901
-```
+"""
 
 # 4.
 print("checking the datatype of int using type() function...")
@@ -42,22 +45,23 @@ print(type(var_1))
 print(type(var_1 + var_2))
 print(type(var_2 + 100))
 
-```python
+"""
+OUTPUT -
 checking the datatype of int using type() function...
 <class 'int'>
 <class 'int'>
 <class 'int'>
-```
-
+"""
 
 # 5. 
 print("FLOAT NUMBERS")
 print(type(var_2 + 300.99999))
 
-```python
+"""
+OUTPUT -
 FLOAT NUMBERS
 <class 'float'>
-```
+"""
 
 # 6.
 print("STRING DATA TYPE")
@@ -71,21 +75,23 @@ print(name)
 print("calling charc from string: ", name[0], name[5])
 print("Intial of her surname: ", name[7])
 
-```python
+"""
+OUTPUT -
 STRING DATA TYPE
 Mihika Pandey
 calling charc from string:  M a
 Intial of her surname:  P
-```
+"""
 
 # 7.
 print(type(name))
 print(type(name + " " + profession))
 
-```python
+"""
+OUTPUT -
 <class 'str'>
 <class 'str'>
-```
+"""
 
 # 8.
 print(name + profession)
@@ -93,11 +99,12 @@ print(name + profession)
 print(name + profession + uid_1)
 print(name + " " + profession + " " + uid_1)
 
-```python
+"""
+OUTPUT -
 Mihika PandeyStudent
 Mihika PandeyStudent10
 Mihika Pandey Student 10
-```
+"""
 
 # 9.
 print("Mihika's book is much \"pretty\"!")
@@ -110,7 +117,8 @@ print(5 * "Mihika is very creative! ") # adding space between next repetation pr
 print()
 print("Mihika is very creative\n" * 5) # printing repetation in next line!
 
-```python
+"""
+OUTPUT -
 Mihika's book is much "pretty"!
 
 Mihik's
@@ -125,15 +133,16 @@ Mihika is very creative
 Mihika is very creative
 Mihika is very creative
 Mihika is very creative
-```
+"""
 
 # 10.
 print("1234" + "56789")
 #print("123" + "456" + 789) will give error
 
-```python
+"""
+OUTPUT -
 123456789
-```
+"""
 
 # 11.
 print("BOOLEAN DATA TYPE")
@@ -141,15 +150,27 @@ var_b = True
 print(var_b)
 print(type(var_b))
 
+# this will give a NameError bec of "true"
+# var_b = true
+# print(var_b)
+# print(type(var_b))
+
 """
-this will give a NameError bec of "true"
-var_b = true
-print(var_b)
-print(type(var_b))
+OUTPUT -
+BOOLEAN DATA TYPE
+True
+<class 'bool'>
 """
-print()
+
+# 12. 
 a = 100
 b = 56
 var_bool = a<b
 print(var_bool)
 print(type(var_bool))
+
+"""
+OUTPUT -
+False
+<class 'bool'>
+"""
