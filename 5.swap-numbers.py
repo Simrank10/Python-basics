140dfe47a633b74d9feb6cbc725909b0c3cdcccc
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-29 23:52:03 +0530

    Update 5.swap-numbers.py

--- 5.swap-numbers.py
+++ 5.swap-numbers.py
@@ -1,8 +1,12 @@
-S1_name = "Swapnil"
+S1_name = "Sophia"
 print(S1_name)
 
 #"a" = 1
 #print("a")
+"""
+Write a Python program to take two numbers as input, swap their values using a temporary variable, 
+and display the numbers before and after swapping.
+"""
 
 no1 = input("Enter value for 1st number:")
 no2 = input("Enter value for 2nd number: ")
@@ -15,3 +19,14 @@ no2 = temp
 print()
 print("no1 after swap: ", no1)
 print("no2 after swap: ", no2)
+
+"""
+OUTPUT -
+Enter value for 1st number:2
+Enter value for 2nd number: 3
+no1 initially:  2
+no2 initially:  3
+
+no1 after swap:  3
+no2 after swap:  2
+"""
