0359755bc23493713760d258131ebb81ff304430
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-01-30 22:58:53 +0530

    Updated 7.add-digits-of-number.py

--- 7.add-digits-of-number.py
+++ 7.add-digits-of-number.py
@@ -1,15 +1,37 @@
+# 1.
 no1 = input("Enter a no.: ")
 no2 = input("Enter a no.: ")
 sum = int(no1) + int(no2)
 print(sum)
 
+"""
+OUTPUT -
+Enter a no.: 23
+Enter a no.: 32
+55
+"""
+
+# 2.
 no = input("Enter a 2 digit number: ")
 first_digit = int(no[0])
 second_digit = int(no[1])
 print("Sum of digits: ", first_digit + second_digit)
 print()
 
+"""
+OUTPUT -
+Enter a 2 digit number: 45
+Sum of digits:  9
+"""
+
+# 3.
 no = input("Enter a 2 digit number: ")
 first_digit = int(no[0])
 second_digit = int(no[1])
 print("Sum of digits: ", first_digit + second_digit)
+
+"""
+OUTPUT - 
+Enter a 2 digit number: 999
+Sum of digits:  18
+"""
