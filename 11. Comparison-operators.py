44cb63e744ebd75c4106ba3255fca45884818803
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-02-03 23:59:25 +0530

    Update 11. Comparison-operators.py

--- 11. Comparison-operators.py
+++ 11. Comparison-operators.py
@@ -21,23 +21,37 @@ print("Not equal to operator -")
 print("y != 50 : ", y != 50)
 print("y != 100 : ", y != 100)
 print("(y+120) != y : ", (y+120) != y)
-print()
+"""
+OUTPUT -
 
+"""
+
+# 4.
 print("Greater than operator - ")
 print("y > 150 : ", y > 150)
 print("y > 50 : ", y > 50)
-print()
+"""
+OUTPUT -
 
+"""
+
+# 5.
 print("Greater than or equal to operator - ")
 print("y >= 110 : ", y >= 110)
-print()
+"""
+OUTPUT -
 
+"""
+
+# 6.
 print("Less than operator - ")
 print("y < 10 : ", y < 10)
 print("y < 200 : ", y < 200)
 print("(y+10)< 100 : ", (y+10)< 100)
-print()
+"""
+OUTPUT -
 
+"""
 print("Less than or equal to operator - ")
 print("y <= 9 : ", y <= 9)
 print()
