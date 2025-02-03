a981503d37ab45302866244a85b8a11cd77d7417
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-02-03 23:26:35 +0530

    Add files via upload

---
+++ input_function.py
@@ -0,0 +1,9 @@
+input("What is your name? ")
+print()
+# Input function will be executed first and later the Print function will be executed
+print("Hello" + " " + input("What's your name? ")) 
+
+
+#Assignment
+
+print("Hey" + " " + input("What's your name?") + ", " "How are you?")
