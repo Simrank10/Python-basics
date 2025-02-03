7d086ac1348b8553855200dbb73a11b59f93200c
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-02-04 00:05:44 +0530

    Update 12. Logical-operators.py

--- 12. Logical-operators.py
+++ 12. Logical-operators.py
@@ -1,22 +1,49 @@
+# 1.
 m, n = 12, 21
 print("m = ", m, ", n = ", n)
-print()
+"""
+OUTPUT -
+m =  12 , n =  21
+"""
 
+# 2.
 print("AND operators in action - ")
 print("m> 12 and n >2 : ", m> 12 and n >2)
 print("m != 10 and n == 10 : ", m != 10 and n == 10)
 print("m <= 100 and n >= 45 : ", m <= 100 and n >= 15)
-print()
+"""
+OUTPUT -
+AND operators in action -
+m> 12 and n >2 :  False
+m != 10 and n == 10 :  False
+m <= 100 and n >= 45 :  True
+"""
 
+# 3.
 print("OR operators in action - ")
 print("m <= 12 or n > 100 : ", m <= 12 or n > 100)
 print("m == 10 or n != 30 : ", m == 10 or n != 30)
 print("m < 5 or n >= 98 : ", m < 5 or n >= 98)
-print()
+"""
+OUTPUT -
+OR operators in action -
+m <= 12 or n > 100 :  True
+m == 10 or n != 30 :  True
+m < 5 or n >= 98 :  False
+"""
 
+# 4.
 print("NOT operator in action - ")
 a = True
 print("a = ", a)
 print("not(a) : ", not(a))
 print("m,n and a : ", m< n and a)
 print("m,n and not(a) : ", m< n and not(a))
+"""
+OUTPUT -
+NOT operator in action -
+a =  True
+not(a) :  False
+m,n and a :  True
+m,n and not(a) :  False
+"""
