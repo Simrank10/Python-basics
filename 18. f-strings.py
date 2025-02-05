27d18d65507b1322b84fa70c460d8ac5930d99b9
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-02-05 23:01:25 +0530

    Update 18. f-strings.py

--- 18. f-strings.py
+++ 18. f-strings.py
@@ -5,13 +5,14 @@ weight = 35
 
 BMI = weight/((height/100)**2)
 
-
+# 1.
 print(f"Her name is {name} and she is {age} years old. She is {height} cms tall and weigh {weight} kgs, and her BMI is {int(BMI)}.")
 """
 OUTPUT -
 Her name is Amaira Trivedi and she is 6 years old. She is 130 cms tall and weigh 35 kgs, and her BMI is 20.
 """
 
+# 2.
 print(f"{name}'s father is {age*5} years old.")
 """
 OUTPUT -
