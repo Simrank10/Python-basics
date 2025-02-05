b62146e717afba6977a15b7d398411e3ccd14a60
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-02-05 22:59:34 +0530

    Updated 17. Round-function.py

--- 17. Round-function.py
+++ 17. Round-function.py
@@ -23,3 +23,31 @@ print("round -1.5 : ", round(-1.5))
 print("round(8, 0) : ", round(8, 0))
 print("round(8) : ", round(8))
 print("round(8.0, 0) : ", round(8.0, 0))
+
+"""
+OUTPUT -
+8
+<class 'int'>
+round 665.123 for 2 digits :  665.12
+round 7.99 for 4 digits :  7.99
+round -19/7 :  -3
+round 9.5 :  10
+round 11.5 :  12
+round 3 to -1 digit :  0
+round 4 to two digits :  4
+round 4 to zero dogit :  4
+round 4 to 1 digit :  4
+round 6.75 to 1 digit :  6.8
+round 675 to -1 digit :  680
+round 674.99200292 to -1 digit :  670.0
+round 674.99200292 to -2 digit :  700.0
+round 674.99200292 to -3 digit :  1000.0
+round 1212 to -2 digits :  1200
+round 143 to - 4 digits :  0
+round 467 to -3 digits :  0
+round 1.5 :  2
+round -1.5 :  -2
+round(8, 0) :  8
+round(8) :  8
+round(8.0, 0) :  8.0
+"""
