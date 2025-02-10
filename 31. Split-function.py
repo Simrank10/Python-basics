f5d7d1339cf5a70ae2b0d1dd9e14fac71724034f
Simran Kanojia <105905798+Simrank10@users.noreply.github.com>
2025-02-10 23:06:40 +0530

    Update 31. Split-function.py

--- 31. Split-function.py
+++ 31. Split-function.py
@@ -4,6 +4,8 @@ splitted_text = text.split(" ")
 print(splitted_text)
 """
 OUTPUT -
+['Welcome', 'to', "Joey's", 'pizza']
+
 names = str(input("PLease enter your team members names seperated by comma : "))
 splitted_names = names.split(", ")
 print(splitted_names)
